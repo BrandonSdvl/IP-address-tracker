c81e5e6f2a5e3b7e7d6eecc5339887e0de20cc9d
BrandonSandoval46 <brandonsandoval194@gmail.com>
2021-10-07 19:29:09 -0500

    Responsive

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -2,24 +2,29 @@ import "./Details.scss";
 
 const Details = ({ dataIp }) => {
   return (
-    <ul className={"details"}>
-      <li className={"details__item"}>
-        IP Address
-        <span>{dataIp.ip}</span>
-      </li>
-      <li className={"details__item"}>
-        Location
-        <span>{dataIp.location}</span>
-      </li>
-      <li className={"details__item"}>
-        Timezone
-        <span>{dataIp.timezone}</span>
-      </li>
-      <li className={"details__item"}>
-        ISP
-        <span>{dataIp.isp}</span>
-      </li>
-    </ul>
+    <div className={"details"}>
+      <ul className={"details__container"}>
+        <li className={"details__item"}>
+          IP Address
+          <span>{dataIp.ip}</span>
+        </li>
+        <div className="separator"></div>
+        <li className={"details__item"}>
+          Location
+          <span>{dataIp.location}</span>
+        </li>
+        <div className="separator"></div>
+        <li className={"details__item"}>
+          Timezone
+          <span>{dataIp.timezone}</span>
+        </li>
+        <div className="separator"></div>
+        <li className={"details__item"}>
+          ISP
+          <span>{dataIp.isp}</span>
+        </li>
+      </ul>
+    </div>
   );
 };
 
